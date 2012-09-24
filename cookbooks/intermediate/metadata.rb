0dbbee18c2a801bc6cb9833da2b405ca09812d9e
jonathansaunders <jonathan.saunders@rightscale.com>
2012-09-24 23:29:00 +0000

    Added a hello world message to the default.rp recipe

---
+++ cookbooks/intermediate/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Jon Co"
+maintainer_email "jon@jonco.com"
+license          "All rights reserved"
+description      "Installs/Configures intermediate"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "<cookbook_name>::default","Prints hello world output"
