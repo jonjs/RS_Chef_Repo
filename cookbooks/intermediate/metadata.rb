9594b99e492c9bd98f21b2aa3944fddfece12c38
jonathansaunders <jonathan.saunders@rightscale.com>
2012-09-25 23:09:37 +0000

    Added rightscale_marker to default.rb and fixed cookbookname error in default

--- cookbooks/intermediate/metadata.rb
+++ cookbooks/intermediate/metadata.rb
@@ -5,4 +5,6 @@ description      "Installs/Configures intermediate"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-recipe "<cookbook_name>::default","Prints hello world output"
+depends "rightscale"
+
+recipe "intermediate::default","Prints hello world output"
