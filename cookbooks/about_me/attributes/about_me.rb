bad2267dabe723ae826f9e2d6a19587ae6737ec7
jonathansaunders <jonathan.saunders@rightscale.com>
2012-09-26 18:03:51 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/attributes/about_me.rb
@@ -0,0 +1,4 @@
+set[:about_me][:first_name] = "Appleseed"
+default[:about_me][:favorite][:food] = "Apple"
+default[:about_me][:favorite][:drink] = "Apple Cider"
+default[:about_me][:favorite][:hobby] = "Picking Apples"
